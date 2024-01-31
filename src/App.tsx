8899d28a6b14c5bc9920d708a0696ca91e9a5c2d
Tyler Johnston <johnstontyler@protonmail.com>
2024-01-31 00:47:50 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
+
+export default function App() {
+  return <MantineProvider theme={theme}>
+
+    </MantineProvider>;
+}
