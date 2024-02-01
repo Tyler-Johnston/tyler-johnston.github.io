086f97de0514c2aa61560f87e94e345ada31a385
Tyler Johnston <johnstontyler@protonmail.com>
2024-01-31 21:18:19 -0700

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { HeaderMenu } from "../components/HeaderMenu/HeaderMenu";
 
 export default function App() {
   return <MantineProvider theme={theme}>
+
+    <HeaderMenu/>
 
     </MantineProvider>;
 }
