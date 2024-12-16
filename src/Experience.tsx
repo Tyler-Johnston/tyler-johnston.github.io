71cf37ca66c1fffa45849e75e574abeb8dd10ffe
Tyler Johnston <johnstontyler@protonmail.com>
2024-12-15 20:18:37 -0700

    Updating Post-Grad Portfolio, pt2

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -2,6 +2,7 @@ import { Container, Title, Paper, Text, Image } from '@mantine/core';
 import blockbuilder from "../images/blockbuilder.jpeg"
 import bucketvector from "../images/bucketvector.png"
 import boundingbox from "../images/boundingbox.png"
+import fp from "../images/footprints.png"
 import { Link } from 'react-router-dom';
 import "../styles.css"
 
@@ -9,6 +10,21 @@ const Experience = () => {
   return (
     <>
       <Container size="md" my={40}>
+      <Paper p="md" my="lg" radius="md">
+        <Title order={3}>Full-Stack Developer | Logan, UT</Title>
+        <Text size="sm" style={{ marginBottom: 10 }}>March 2024 - Present</Text>
+        <Text>I work as a full-stack developer, contributing to both the front-end and back-end of applications used by engineering teams in the aerospace and steel manufacturing industries. These applications are designed to help users plan, organize, and track critical workflows, ensuring efficiency and precision in highly technical environments.</Text>
+        <br/>
+        <Text>On the back end, I use .NET (C#) with .ashx handler files, AJAX calls, and Oracle SQL, leveraging SQL Developer for database management. On the front end, I utilize JavaScript frameworks such as Ext JS and jQuery to build responsive user interfaces that interact seamlessly with the backend systems.</Text>
+        <br/>
+        <Text>In addition to development tasks, I frequently perform Quality Assurance (QA) checks to ensure the validity and correctness of the code produced by myself and other developers. This includes debugging, optimizing performance, and adhering to coding standards to maintain high-quality deliverables for clients.</Text>
+        <br/>
+        <Text>Collaboration is an integral part of my role, as I work closely with a team of developers to solve complex problems and deliver projects on schedule. My strong communication skills allow me to effectively discuss technical details, gather requirements, and provide feedback, fostering a productive and supportive team environment.</Text>
+        <br/>
+        <Image className="smallImage" src={fp}/>
+    </Paper>
+
+
         <Paper p="md" my="lg" radius="md">
           <Title order={3}>Computer Vision Research Assistant, Utah State University | Logan, UT</Title>
           <Text size="sm" style={{ marginBottom: 10 }}>January 2024 - May 2024</Text>
@@ -36,7 +52,7 @@ const Experience = () => {
           <Text> Under the direction of Utah State University's Analytics Solutions Center, I assisted in the development of a custom web user-interface for Space Dynamics Laboratory (SDL). We utilized React.js for the front end, and C#/.NET for the backend. 
             I also leveraged Docker containers to streamline block data extraction, and I had the opportunity to design this web application using the Astro Space UX Design System.</Text>
           <br/>
-          <Text> Originally, for the development of their proprietary software, SDL required developers to hand-write and edit XML documents. As these were often nested and had several layers of parameters, this was incredibly tedious and error-prone. BlockBuilder allows developers to drag-and-drop blocks, connect them with each other, and edit their parameters in a clean user-interface. Afterwards, these processes can be exported in either JSON or XML format to be utilized in their proprietary applications</Text>
+          <Text>Originally, for the development of their proprietary software, SDL required developers to hand-write and edit XML documents. As these were often nested and had several layers of parameters, this was incredibly tedious and error-prone. BlockBuilder allows developers to drag-and-drop blocks, connect them with each other, and edit their parameters in a clean user-interface. Afterwards, these processes can be exported in either JSON or XML format to be utilized in their proprietary applications</Text>
           <br/>
           <Text>I had the opportunity to present my work at SDL. Meeting with developers who would be impacted by our work was incredibly rewarding. They told us this would be extremely helpful in reducing bugs and increasing productivity.</Text>
           <Image className="mediumImage" src={blockbuilder}/>
--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,55 +1,85 @@
-import { Container, Grid, Card, Text, Title, Button, Image } from '@mantine/core';
-import vintagefinds1 from "../images/vintagefinds1.png"
-import reptiletracker1 from "../images/reptiletracker1.png"
-import cartpole1 from "../images/cartpole1.png"
-import aws from "../images/aws.png"
-import facialexpression from "../images/facialexpression.png"
-import yolo from "../images/yolo.png"
+import { Container, Card, Text, Title, Button, Image, Accordion, Grid } from '@mantine/core';
+import vintagefinds from "../images/vintagefinds.png";
+import reptiletracker from "../images/reptiletracker.png";
+import cartpole1 from "../images/cartpole1.png";
+import snakegame from "../images/snakegame.png";
+import lunarlander from "../images/lunarlander.png";
+import aws from "../images/aws.png";
+import facialexpression from "../images/facialexpression.png";
+import maze from "../images/maze.png";
+import yolo from "../images/yolo.png";
 
-const projects = [
-  {
-    id: 1,
-    title: 'Vintage Finds',
-    description: 'This is a website I started developing for my grandfather. He owns an antique booth and wanted a way to let his customers know what items he had in stock. I developed this using Next.js and Google\'s relational database.',
-    imageUrl: vintagefinds1,
-    projectUrl: 'https://github.com/Tyler-Johnston/Vintage-Finds'
-  },
-  {
-    id: 2,
-    title: 'Reptile Tracker',
-    description: 'This is a website to manage one\'s pet reptile\'s schedules, husbandry, and feedings. I developed the back-end using Express.js and the front-end using React.js.',
-    imageUrl: reptiletracker1,
-    projectUrl: 'https://github.com/Tyler-Johnston/Reptile-Tracker'
-  },
-  {
-    id: 3,
-    title: 'AWS Widget Requests',
-    description: 'Designed and implemented both Producer and Consumer applications with AWS, responsible for generating user widgets, queuing them via SQS, processing the data, and persisting results in either S3 buckets or DynamoDB tables.',
-    imageUrl: aws,
-    projectUrl: 'https://github.com/Tyler-Johnston/cs5260-AWS-Widget-Requests'
-  },
-  {
-    id: 4,
-    title: 'Cartpole Reinforcement Learning',
-    description: 'Developed a reinforcement learning (ANN) model for OpenAI\'s cart pole using DeepQLearning.',
-    imageUrl: cartpole1,
-    projectUrl: 'https://github.com/Tyler-Johnston/cs5600-cartpole-reinforcement-learning'
-  },
-  {
-    id: 5,
-    title: 'Beehive Weight Prediction using Machine Learning',
-    description: 'Developed Artificial Neural Network (ANN), Convolutional Neural Network (ConvNet), and Long Short‐Term Memory (LSTM) models for the purpose of forecasting beehive weight based on its internal temperature using Keras. Developed ResNet50 and YOLO ConvNet models for bee/non‐bee image classification using PyTorch.',
-    imageUrl: yolo,
-    projectUrl: 'https://github.com/Tyler-Johnston/cs5600-Beehive-Weight-Prediction'
-  },
-  {
-    id: 6,
-    title: 'Facial Expression Recognition',
-    description: 'Developed a SVM model to recognize facial expressions baseds on Local Binary Patterns and Oriented FAST and rotated BRIEF features',
-    imageUrl: facialexpression,
-    projectUrl: 'https://github.com/Tyler-Johnston/cs5680-facial-expression-recognition'
-  },
-];
+const projects = {
+  gameDev: [
+    {
+      id: 0,
+      title: 'Multiplayer Snake Game',
+      description: 'A modern, competitive take on the classic Snake experience, designed for real-time, multiplayer gameplay. Players aim to grow the longest snake while navigating an arena filled with other players.',
+      imageUrl: snakegame,
+      projectUrl: 'https://github.com/Tyler-Johnston/Multiplayer-Snake-Game'
+    },
+    {
+      id: 1,
+      title: 'Lunar Lander Game',
+      description: "A physics-based game where the player controls a lunar module's descent, adjusting thrust and angle to land safely on the moon's surface. The player must carefully balance speed and fuel to avoid crashing.",
+      imageUrl: lunarlander,
+      projectUrl: 'https://github.com/Tyler-Johnston/Lunar-Lander'
+    },
+    {
+      id: 2,
+      title: 'Maze Game',
+      description: 'A maze game with guaranteed, procedurally generated paths from one corner of the map to the other.',
+      imageUrl: maze,
+      projectUrl: 'https://github.com/Tyler-Johnston/Maze-Game'
+    }
+  ],
+  webDev: [
+    {
+      id: 3,
+      title: 'Vintage Finds',
+      description: 'This is a website I started developing for my grandfather. He owns an antique booth and wanted a way to let his customers know what items he had in stock. I developed this using Next.js and Google\'s relational database.',
+      imageUrl: vintagefinds,
+      projectUrl: 'https://github.com/Tyler-Johnston/Vintage-Finds'
+    },
+    {
+      id: 4,
+      title: 'Reptile Tracker',
+      description: 'This is a website to manage one\'s pet reptile\'s schedules, husbandry, and feedings. I developed the back-end using Express.js and the front-end using React.js.',
+      imageUrl: reptiletracker,
+      projectUrl: 'https://github.com/Tyler-Johnston/Reptile-Tracker'
+    },
+    {
+      id: 5,
+      title: 'AWS Widget Requests',
+      description: 'Designed and implemented both Producer and Consumer applications with AWS, responsible for generating user widgets, queuing them via SQS, processing the data, and persisting results in either S3 buckets or DynamoDB tables.',
+      imageUrl: aws,
+      projectUrl: 'https://github.com/Tyler-Johnston/cs5260-AWS-Widget-Requests'
+    }
+  ],
+  machineLearning: [
+    {
+      id: 6,
+      title: 'Cartpole Reinforcement Learning',
+      description: 'Developed a reinforcement learning (ANN) model for OpenAI\'s cart pole using DeepQLearning.',
+      imageUrl: cartpole1,
+      projectUrl: 'https://github.com/Tyler-Johnston/cs5600-cartpole-reinforcement-learning'
+    },
+    {
+      id: 7,
+      title: 'Beehive Weight Prediction using Machine Learning',
+      description: 'Developed Artificial Neural Network (ANN), Convolutional Neural Network (ConvNet), and Long Short‐Term Memory (LSTM) models for the purpose of forecasting beehive weight based on its internal temperature using Keras. Developed ResNet50 and YOLO ConvNet models for bee/non‐bee image classification using PyTorch.',
+      imageUrl: yolo,
+      projectUrl: 'https://github.com/Tyler-Johnston/cs5600-Beehive-Weight-Prediction'
+    },
+    {
+      id: 8,
+      title: 'Facial Expression Recognition',
+      description: 'Developed a SVM model to recognize facial expressions based on Local Binary Patterns and Oriented FAST and rotated BRIEF features.',
+      imageUrl: facialexpression,
+      projectUrl: 'https://github.com/Tyler-Johnston/cs5680-facial-expression-recognition'
+    }
+  ]
+};
 
 const Portfolio = () => {
   return (
@@ -58,23 +88,44 @@ const Portfolio = () => {
       <Text size="lg" mb="xl">
         Welcome to my portfolio. Here you'll find a selection of my work and projects.
       </Text>
-      
-      <Grid>
-        {projects.map((project) => (
-          <Grid.Col span={12} key={project.id}>
-            <Card shadow="sm" padding="lg">
-              <Card.Section>
-                <Image src={project.imageUrl} alt={project.title} height={160} />
-              </Card.Section>
-              <Text size="lg" mt="md">{project.title}</Text>
-              <Text size="sm" mt="md">{project.description}</Text>
-              <Button variant="light" color="blue" fullWidth mt="md" component="a" href={project.projectUrl} target="_blank">
-                View Project
-              </Button>
-            </Card>
-          </Grid.Col>
+
+      <Accordion multiple defaultValue={['gameDev', 'webDev', 'machineLearning']}>
+        {Object.entries(projects).map(([category, categoryProjects]) => (
+          <Accordion.Item value={category} key={category}>
+            <Accordion.Control>
+              {category === 'gameDev' && 'Game Development'}
+              {category === 'webDev' && 'Web Development'}
+              {category === 'machineLearning' && 'Machine Learning'}
+            </Accordion.Control>
+            <Accordion.Panel>
+              <Grid>
+                {categoryProjects.map((project) => (
+                  <Grid.Col span={12} key={project.id}>
+                    <Card shadow="sm" padding="lg">
+                      <Card.Section>
+                        <Image src={project.imageUrl} alt={project.title} height={160} />
+                      </Card.Section>
+                      <Text size="lg" mt="md">{project.title}</Text>
+                      <Text size="sm" mt="md">{project.description}</Text>
+                      <Button
+                        variant="light"
+                        color="blue"
+                        fullWidth
+                        mt="md"
+                        component="a"
+                        href={project.projectUrl}
+                        target="_blank"
+                      >
+                        View Project
+                      </Button>
+                    </Card>
+                  </Grid.Col>
+                ))}
+              </Grid>
+            </Accordion.Panel>
+          </Accordion.Item>
         ))}
-      </Grid>
+      </Accordion>
     </Container>
   );
 };
